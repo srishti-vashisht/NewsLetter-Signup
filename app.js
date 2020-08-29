032067ee03863617b6a3ef8ade3cfab52cb2bd82
Srishti <srishti.vashisht@outlook.com>
2020-08-29 05:33:31 +0530

    Second

--- app.js
+++ app.js
@@ -7,12 +7,25 @@ const app = express();        /*tells that we are making an express application*
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(express.static("public"));                                /*Public folder has all static or remote files which needs to accessed on server, use static function from express to serve static files to server */
+
+
 app.get("/",function(req,res){
 
-    res.send("Welcome");
+    res.sendFile(__dirname+"/signup.html");                    /*When home page is loaded - render signup.html page on website*/
 
 })
 
+app.post("/",function(req,res){
+
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var Email = req.body.Email;
+    console.log(mail);
+})
+
+/*
+ba234c27c662811c98c8604b33b14ffc-us17
 
 app.listen(3000,function(req,res){
     console.log("Server is running on port 3000");
